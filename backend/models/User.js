45c1fa7b499fb36ab1b3adc02aee89f4bfe4ccc9
Eduardo <eduher.rodp@gmail.com>
2024-06-09 18:51:28 -0600

    models User, Video

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    favorites: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    }]
+});
+
+module.exports = User = mongoose.model('User',UserSchema);
---
+++ backend/models/Video.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const VideoSchema = new mongoose.Schema({
+    videoId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    url: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
