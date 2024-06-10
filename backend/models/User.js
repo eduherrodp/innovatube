f2df18d97e341c0b86a06053b62ae247737376ba
Eduardo <eduher.rodp@gmail.com>
2024-06-09 23:59:39 -0600

    backend connection - routes and controllers /fix login User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,6 +10,11 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     username: {
         type: String,
         required: true,
